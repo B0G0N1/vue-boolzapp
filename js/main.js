1c4b1ec23ec701498eda452d9ee2af1ec8d1c1fb
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-22 20:12:45 +0200

    [FEAT] MILESTONE 2: Add contact click

--- js/main.js
+++ js/main.js
@@ -164,5 +164,18 @@ createApp({
 			// Messaggio attivo all'avvio
 			lastMessage: 0
 		};
+	},
+	methods: {
+		// Metodo per formattare data ore & minuti
+		formatDate(dateString) {
+			const date = new Date(dateString);
+			const hours = String(date.getHours()).padStart(2, '0');
+			const minutes = String(date.getMinutes()).padStart(2, '0');
+			return `${hours}:${minutes}`;
+		},
+		// Metodo per cambiare chat attiva
+		setActiveChat(index) {
+			this.activeChat = index;
+		}
 	}
 }).mount('#app');
